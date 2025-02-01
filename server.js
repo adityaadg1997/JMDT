b0db9354509def8e499d02b4bf810167bfff4f87
adityaadg1997 <adityaadg1997@gmail.com>
2025-02-01 07:24:28 +0530

    initial commit 1

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running!");
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Handle all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
